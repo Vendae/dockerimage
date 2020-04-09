9b1e2e8012c2411781f823ca88acb5454f4348c1
hanumantharao19 <63094959+hanumantharao19@users.noreply.github.com>
2020-04-10 01:24:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('this is hanumantharao medikonda\n');
+  res.send('docker image from terraform team \n');
 });
 
 app.listen(PORT, HOST);
