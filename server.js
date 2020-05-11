73892574c47fe9c2e4db759af59d16e73f6ac9ed
hanumantharao19 <63094959+hanumantharao19@users.noreply.github.com>
2020-05-11 17:31:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('packer2 image from terraform team \n');
+  res.send('packer3 image from terraform team \n');
 });
 
 app.listen(PORT, HOST);
